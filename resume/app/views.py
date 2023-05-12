f5e584566c8ad23e5f1c878b26bddd67f5e2ed40
Loyolan <kiadyyrann@gmail.com>
2023-05-13 02:11:24 +0300

    DARK AND LIGHT

--- resume/app/views.py
+++ resume/app/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def show_cv(r):
-    
-    return render(r, 'index.html')
+    theme = r.GET.get('theme')
+    current_url = r.path
+    params = {'svg': 'light', 'change': 'Light Mode', 'link': f'{current_url}?theme=light'}
+    if theme is not None:
+        if theme == 'light':
+            params = {'svg': 'dark', 'change': 'Dark Mode', 'link': f'{current_url}?theme=dark'}
+    data = {'params': params}
+    return render(r, 'index.html', data)
 
 def index(r):
     return redirect('/resume/show/')
