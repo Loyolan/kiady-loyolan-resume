0f22ee459b8d60c2a198e308422b181a23b5bb8f
Loyolan <kiadyyrann@gmail.com>
2023-05-12 16:57:06 +0300

    database configuration

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -73,11 +73,13 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ypds$resume',
+        'USER': 'ypds',
+        'PASSWORD': 'Mot de passe2079',
+        'HOST': 'ypds.mysql.pythonanywhere-services.com',
     }
 }
 
