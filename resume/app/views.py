0466ec9c911f4ed054d89ed0a56d84604dcad06b
Loyolan <kiadyyrann@gmail.com>
2023-05-12 16:01:30 +0300

    DJANGO INTEGRATION

---
+++ resume/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def show_cv(r):
+    return render(r, 'index.html')
+
+def index(r):
+    return redirect('/resume/show/')
