38178905d3173d747910028aabbb4568ab1d945b
Loyolan <kiadyyrann@gmail.com>
2023-05-13 14:52:37 +0300

    ICON THEME

--- resume/app/utils.py
+++ resume/app/utils.py
@@ -1,6 +1,6 @@
 def change_theme(th, url):
-    params = {'svg': 'light', 'change': 'Light Mode', 'link': f'{url}?theme=light'}
+    params = {'svg': 'light', 'change': 'Light', 'link': f'{url}?theme=light'}
     if th is not None:
         if th == 'light':
-            params = {'svg': 'dark', 'change': 'Dark Mode', 'link': f'{url}?theme=dark'}
+            params = {'svg': 'dark', 'change': 'Dark', 'link': f'{url}?theme=dark'}
     return params
